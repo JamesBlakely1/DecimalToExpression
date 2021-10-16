bb89088d92f51197520b52d65a22c068b125ee0a
James Blakely <jamesblakely@Jamess-MacBook-Pro.local>
2021-10-15 19:28:18 -0700

    hello

--- bruteforcedivision.py
+++ bruteforcedivision.py
@@ -1,2 +1,8 @@
 def bruteforcedivision(decimal):
-	return "hi"
+	for x in range(0, 100):
+		for y in range(1, 100):
+			if (1.0 * x/y) - .001 < decimal and (1.0 * x/y) + .001 > decimal:
+			#if (1.0 * x/ 1.0 * y) == decimal:
+				print x
+				print y
+				return " "
